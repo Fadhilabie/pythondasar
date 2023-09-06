17fd608b81fa584c03d233c49bfffebc09dbd495
Fadhilabie <132025341+Fadhilabie@users.noreply.github.com>
2023-09-06 18:47:58 +0700

    modulepy

---
+++ 3.math.py
@@ -0,0 +1,17 @@
+# Memberikan akses ke berbagai fungsi matematika dan konstanta matematika yang umum digunakan.
+import math
+
+# Menghitung akar kuadrat
+akar = math.sqrt(16)
+print(akar)
+
+# Menghitung logaritma basis 10
+log_10 = math.log10(100)
+print(log_10)
+
+# Menghitung sin, cos, dan tan
+sudut = math.radians(30)  # Mengubah sudut ke radian
+sin = math.sin(sudut)
+cos = math.cos(sudut)
+tan = math.tan(sudut)
+print(sin, cos, tan)
