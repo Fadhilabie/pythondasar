80a827d13c897e0e0a515306e166f4e3a4b3040c
Fadhilabie <fadhilmuhhab@gmail.com>
2023-09-26 20:46:25 +0700

    pp

--- OOP/LatihanSoal.py
+++ OOP/LatihanSoal.py
@@ -1,42 +1,42 @@
-class Contact:
-    def __init__(self, name, phone):
-        self.name = name
-        self.phone = phone
-
-    def get_name(self):
-        # Lengkapi metode ini untuk mengembalikan nama kontak
-        return self.name
-
-    def get_phone(self):
-        # Lengkapi metode ini untuk mengembalikan nomor telepon kontak
-        return self.phone
-
-    def __str__(self):
-        return f"{self.name}: {self.phone}"
-
-
 class ContactList:
     def __init__(self):
         # Definisikan atribut/variabel daftar kontak dari fungsi init sebagai List.
-        self.contacts = ""
-        pass
+        self.contacts = []
 
     def add_contact(self, contact):
         # Lengkapi metode ini untuk menambahkan kontak ke dalam daftar kontak
-        pass
+        self.contacts.append(contact)
 
     def delete_contact(self, name):
         # Lengkapi metode ini untuk menghapus kontak berdasarkan nama
-        pass
+        deleted = False
+        for contact in self.contacts:
+            if contact.get_name() == name:
+                self.contacts.remove(contact)
+                deleted = True
+                print(f"Kontak {name} telah dihapus.")
+                break
+        if not deleted:
+            print(f"Kontak dengan nama {name} tidak ditemukan.")
 
     def search_contact(self, name):
         # Lengkapi metode ini untuk mencari kontak berdasarkan nama
-        pass
+        found = False
+        for contact in self.contacts:
+            if contact.get_name() == name:
+                print(f"Nama: {contact.get_name()}\nNomor Telepon: {contact.get_phone()}")
+                found = True
+                break
+        if not found:
+            print(f"Kontak dengan nama {name} tidak ditemukan.")
 
     def display_contacts(self):
         # Lengkapi metode ini untuk menampilkan semua kontak dalam daftar
-        pass
-
+        if not self.contacts:
+            print("Daftar kontak kosong.")
+        else:
+            for contact in self.contacts:
+                print(contact)
 
 # Membuat objek daftar kontak
 contact_list = ContactList()
@@ -54,25 +54,20 @@ while True:
     if choice == "1":
         name = input("Masukkan nama kontak: ")
         phone = input("Masukkan nomor telepon kontak: ")
-        contact = Contact(name, phone)
+        contact = contact(name, phone)
         contact_list.add_contact(contact)
         print(f"Kontak {name} telah ditambahkan.")
 
     elif choice == "2":
         name = input("Masukkan nama kontak yang akan dihapus: ")
-        # Panggil metode untuk menghapus kontak berdasarkan nama
-        contact_list.delete_contact(contact)
-        print(f"Kontak {name} telah dihapus")
-        pass
+        contact_list.delete_contact(name)
 
     elif choice == "3":
         name = input("Masukkan nama kontak yang akan dicari: ")
-        # Panggil metode untuk mencari kontak berdasarkan nama
-        pass
+        contact_list.search_contact(name)
 
     elif choice == "4":
-        # Panggil metode untuk menampilkan semua kontak
-        pass
+        contact_list.display_contacts()
 
     elif choice == "5":
         print("Terima kasih! Program selesai.")
