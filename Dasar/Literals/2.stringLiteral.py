aeabdb963ee5ac761bf3e4ec041a6ee0c8f19c78
fadhilabie <fadhilmuhhab@gmail.com>
2024-04-30 21:33:06 +0700

    Menambahkan Literals

---
+++ Dasar/Literals/2.stringLiteral.py
@@ -0,0 +1,7 @@
+nama_abie = 'fadhil muhammad habibie'
+nama_rakha = "rakha nayottama"
+nama_rifai = '''Ahmad
+                    Rifai
+                        Pai'''
+
+print(nama_abie, nama_rakha, nama_rifai)
---
+++ Dasar/Literals/3.numericLiteral.py
@@ -0,0 +1,26 @@
+# Integer Literal
+
+# Positive
+p = 123
+# Negative
+n = -123
+# Nol
+z = 0
+# Bilangan floating-point
+f = 3.15
+# Bilangan floating-point positif yang ditulis dalam notasi ilmiah
+fp = 4.2e-3
+# Binary
+a = 0b10100
+# Decimal
+b = 50
+# Octal
+c = 0o320
+# Hexadecimal
+d = 0x12b
+# Bilangan kompleks dengan bagian real 2 dan bagian imajiner 3
+k = 2 + 3j
+# Bilangan kompleks dengan bagian real -1.5 dan bagian imajiner -2.5
+kr = 1.5 - 2.5j
+
+print(p, n, z, f, fp, a, b, c, d, k, kr)
